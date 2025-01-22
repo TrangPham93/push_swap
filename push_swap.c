8579a58e79007fb81073f3d780aaff405d11a3f0
TrangPham93 <hatrangc2@gmail.com>
2025-01-22 23:33:30 +0200

    min_node

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: trpham <trpham@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/13 10:34:57 by trpham            #+#    #+#             */
-/*   Updated: 2025/01/17 21:46:15 by trpham           ###   ########.fr       */
+/*   Updated: 2025/01/22 18:21:13 by trpham           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,9 @@
 #include "./libft/includes/libft.h"
 #include "./libft/includes/ft_printf.h"
 #include <stdint.h>
+
+void	quick_sort(t_node **stack_a, t_node **stack_b);
+int	min_node(t_node *lst);
 
 char	**ft_handle_input(int argc, char *argv[])
 {
@@ -47,6 +50,13 @@ char	**ft_handle_input(int argc, char *argv[])
 	return (input_arr);
 }
 
+t_node	*ft_sort(t_node *stack_a)
+{
+	
+	
+	return (stack_a);	
+}
+
 int	main(int argc, char *argv[])
 {
 	t_node	*stack_a;
@@ -62,23 +72,64 @@ int	main(int argc, char *argv[])
 		return (-1);
 	}
 	stack_a = create_stack(input_arr);
-	// test function
+	ft_sort(stack_a);
+	// print list
 	printf("Print original lst:\n");
 	print_list(stack_a);
-	stack_a = swap_stack(stack_a);
-	printf("After swap:\n");
-	print_list(stack_a);
-	printf("ROTATION:\n");
-	stack_a = rotate_stack(stack_a);
-	print_list(stack_a);
-	printf("Reversed ROTATION:\n");
-	stack_a = reverse_rotate(stack_a);
-	print_list(stack_a);
-	printf("PUSH\n");
-	push_stack(&stack_a, &stack_b);
-	print_list(stack_a);
-	print_list(stack_b);
-	free_list(stack_a);
-	free_list(stack_b);
+	// printf("%d\n", min_node(stack_a));
+	quick_sort(&stack_a, &stack_b);
+	// test function
+	// 
+	// stack_a = swap_stack(stack_a);
+	// printf("After swap:\n");
+	// print_list(stack_a);
+	// printf("ROTATION:\n");
+	// stack_a = rotate_stack(stack_a);
+	// print_list(stack_a);
+	// printf("Reversed ROTATION:\n");
+	// stack_a = reverse_rotate(stack_a);
+	// print_list(stack_a);
+	// printf("PUSH\n");
+	// push_stack(&stack_a, &stack_b);
+	// push_stack(&stack_a, &stack_b);
+	// print_list(stack_a);
+	// print_list(stack_b);
+	// free_list(stack_a);
+	// free_list(stack_b);
 	return (0);
+}
+
+void	quick_sort(t_node **stack_a, t_node **stack_b)
+{
+	int	min;
+	t_node	*head;
+
+	head = NULL;
+	
+	if (node_lst_size(*stack_a) == 1)
+		return ;
+	min = min_node(*stack_a);
+	ft_printf("Min in stack a: %d\n", min);
+	if (head->content != min)
+	
+	push_stack(stack_a, stack_b);
+}
+
+int	min_node(t_node *lst)
+{
+	int		min_value;
+	int		min_index = 1;
+	t_node	*temp;
+
+	if (lst != NULL)
+		min_value = lst->content;
+	// temp = lst;
+	while (lst->next)
+	{
+		// temp = temp->next;
+		
+		if (min_value > temp->content)
+			min_value = temp->content;
+	}
+	return (min_value);
 }
